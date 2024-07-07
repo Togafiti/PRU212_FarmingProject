9f3ee282a277dd9ca43df8b4abe89c9ae6a23043
saodoqw <98745523+saodoqw@users.noreply.github.com>
2024-07-08 05:24:58 +0700

    dialouge system missing ep3

---
+++ Assets/Scripts/Actor.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "Data/Dialogue/Actor")]
+public class Actor : ScriptableObject
+{
+    // Start is called before the first frame update
+   public string Name;
+    public Sprite portrait;
+}
